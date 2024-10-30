779d37321dacfd89e7be036216a0852d3c5f653f
learningwithas <learningwithas@gmail.com>
2024-10-30 16:43:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Accordium from "./components/accordium/Accordium";
+
+const App = () => {
+  return (
+    <h1 className="">
+      <Accordium />
+    </h1>
+  );
+};
+
+export default App;
---
+++ src/components/accordium/Accordium.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import data from "./data";
+
+import "./Accordium.css";
+
+const Accordium = () => {
+  return <div className="accordium text-center mt-3">Accordium</div>;
+};
+
+export default Accordium;
